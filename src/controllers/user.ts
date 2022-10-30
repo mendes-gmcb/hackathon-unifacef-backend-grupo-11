eedfa19c4f1c908d580a4b12678c1a30b802021b
gmcb_mendes <gmendescamargodebarros@gmail.com>
2022-10-29 23:05:02 -0300

    atualização da funcionalidade de manipulação do user

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,4 +1,4 @@
-import { Controller, Post } from "@overnightjs/core";
+import { Controller, Delete, Get, Post, Put } from "@overnightjs/core";
 import { Request, Response } from "express";
 import { User } from "@src/model/user";
 import AuthService from "@src/services/authService";
@@ -11,11 +11,88 @@ export class UsersController extends BaseController {
   public async create(req: Request, res: Response): Promise<void> {
     // rota para criar novo usuario
     try {
+      req.body.cnpj = await req.body.cnpj.replace(/[^0-9]/g, "");
+
       const user = new User(req.body);
       const newUser = await user.save();
       res.status(201).send(newUser);
     } catch (e) {
       this.sendCreateUpdateErrorResponse(res, e);
+    }
+  }
+
+  @Get("get/:cnpj")
+  public async get(
+    req: Request,
+    res: Response
+  ): Promise<Response | undefined> {
+    // rota para pegar o id do usuário
+    const { cnpj } = req.params; 
+
+    const user = await User.findOne({ cnpj: cnpj });
+
+    if (!user) {
+      return res
+        .status(401)
+        .send({ code: 401, message: "Usuario não encontrado" });
+    }
+
+    user.cnpj = user.cnpj.replace(/^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, "$1.$2.$3/$4-$5");
+
+    return res.status(200).send({ user });
+  }
+
+  @Put("update/:id")
+  public async update(req: Request, res: Response): Promise<Response | undefined> {
+    // rota para atualizar um usuário
+    try {
+      const { id } = req.params;
+      const { name, password } = req.body;
+
+      if (!password) {
+        return;
+      }
+    
+      try {
+        const hashedPassword = await AuthService.hashPassword(password);
+        req.body.password = hashedPassword;
+      } catch (err) {
+        console.error(`Erro no hash do password do usuario ${name}`);
+        // @TODO ERRO
+      }
+
+      try {
+        let user = await User.findByIdAndUpdate(id, req.body);
+        user = await User.findById(id);
+
+        if (!user) {
+          return res.status(401).send({code: 401, message: "Usuário não encontrado"});
+        }
+
+        user.cnpj = user.cnpj.replace(/^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, "$1.$2.$3/$4-$5");
+  
+        return res.status(201).send(user);
+      } catch (e) {
+        return res
+          .status(401)
+          .send({ code: 401, message: "Usuario não encontrado"});
+      }
+    } catch (e) {
+      this.sendCreateUpdateErrorResponse(res, e);
+      return;
+    }
+  }
+
+  @Delete("delete/:id")
+  public async delete(req: Request, res: Response): Promise<Response | undefined> {
+    // rota para excluir um usuário
+    try {
+      const { id } = req.params;
+      const user = await User.deleteOne({ id: id });
+      return res.status(201).send(user);
+    } catch (e) {
+      this.SendErrorResponse(res, e);
+      return;
     }
   }
 
--- src/model/user.ts
+++ src/model/user.ts
@@ -3,9 +3,14 @@ import Authservice from "@src/services/authService";
 
 export interface User {
   _id?: string;
-  name: string;
-  email: string;
+  name?: string;
+  email?: string;
   password: string;
+  cnpj: string;
+  razao_social?: string;
+  termos?: boolean;
+  whatsapp?: string;
+  userContabilId: string;
 }
 
 export enum CUSTOM_VALIDATION {
@@ -16,9 +21,14 @@ interface UserModel extends Omit<User, "_id">, Document {}
 
 const schema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    name: { type: String, required: false },
+    email: { type: String, required: false },
+    password: { type: String, required: false },
+    cnpj: { type: String, required: true },
+    razao_social: { type: String, required: true },
+    termos: { type: Boolean, required: true, default: false },
+    whatsapp: { type: String, required: false },
+    userContabilId: { type: String, required: true }
   },
   {
     toJSON: {
@@ -39,9 +49,27 @@ schema.path("email").validate(
   "already exists in the database.",
   CUSTOM_VALIDATION.DUPLICATED
 );
+
+schema.path("cnpj").validate(
+  async (cnpj: string) => {
+    const cnpjCount = await mongoose.models.User.countDocuments({ cnpj });
+    return !cnpjCount;
+  },
+  "already exists in the database.",
+  CUSTOM_VALIDATION.DUPLICATED
+);
+
+schema.path("razao_social").validate(
+  async (razao_social: string) => {
+    const razaoSocialCount = await mongoose.models.User.countDocuments({ razao_social });
+    return !razaoSocialCount;
+  },
+  "already exists in the database.",
+  CUSTOM_VALIDATION.DUPLICATED
+);
+
 // trás o erro da informação duplicada para a camada do mongoose
 // fazendo ele parar de retornar um erro diretamenta do MongoDB
-
 schema.pre<UserModel>("save", async function (): Promise<void> {
   if (!this.password || !this.isModified("password")) {
     return;
